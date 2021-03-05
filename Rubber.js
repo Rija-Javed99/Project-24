abc1cd9cae4b7b138857530645f9a23f216503d8
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-03-05 16:50:53 +0530

    Update Rubber.js

--- Rubber.js
+++ Rubber.js
@@ -17,12 +17,10 @@ class Rubber{
 	{
 			var rubberpos=this.body.position;		
 			push()
-			translate(rubberpos.x, rubberpos.y);
-			rectMode(CENTER)
-			strokeWeight(4);
-			stroke("black");
-			fill("darkblue");
-			ellipse(0,0,this.r, this.r);
+			
+		//set display properties
+		
+		
 			pop()
 	}
 
